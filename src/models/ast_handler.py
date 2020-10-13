7f0608ae6046d90d9e97daf1534cf4c6fb603517
niksaz <nikitasazanovich@gmail.com>
2020-10-13 13:15:23 +0300

    Bag of types is appended to the original code tokens sequence.

--- src/models/ast_handler.py
+++ src/models/ast_handler.py
@@ -4,27 +4,37 @@ from typing import List
 
 from dpu_utils.utils import RichPath
 
+TreeNode = collections.OrderedDict
+
 
 def query_to_raw_tree_path(file_path: RichPath):
   raw_tree_path = file_path.__str__().replace('/python/', '/python_raw_trees/')
   return RichPath.create(raw_tree_path)
 
 
-def _add_code_tokens_from_node(node: collections.OrderedDict, tokens: List[str]):
-  node_string = node['string']
-  python_identifier = re.compile(r'^[^\d\W]\w*\Z', re.UNICODE)
-  if re.match(python_identifier, node_string):
-    tokens.append(node_string)
+def _linearize_tree(node: TreeNode, linearization: List[TreeNode]):
+  linearization.append(node)
   for child in node['children']:
-    _add_code_tokens_from_node(child, tokens)
+    _linearize_tree(child, linearization)
 
 
-def get_code_tokens_from_tree(tree: collections.OrderedDict):
-  code_tokens = []
-  _add_code_tokens_from_node(tree, code_tokens)
+def _get_code_tokens_from_tree(tree: collections.OrderedDict):
+  linearization = []
+  _linearize_tree(tree, linearization)
+  node_tokens = list(map(lambda node: node['string'], linearization))
+  python_identifier_pattern = re.compile(r'^[^\d\W]\w*\Z', re.UNICODE)
+  code_tokens = list(filter(lambda token: re.match(python_identifier_pattern, token), node_tokens))
   return code_tokens
 
 
+def _get_types_bag_from_tree(tree: collections.OrderedDict):
+  linearization = []
+  _linearize_tree(tree, linearization)
+  type_tokens = list(map(lambda node: node['type'], linearization))
+  types_bag = set(type_tokens)
+  return types_bag
+
+
 def mix_raw_tree_in(raw_sample, raw_tree):
-  raw_sample['code_tokens'] = get_code_tokens_from_tree(raw_tree)
+  raw_sample['code_tokens'].extend(_get_types_bag_from_tree(raw_tree))
   return raw_sample
