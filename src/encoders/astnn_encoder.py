733989be9a47e57f8444b2573ce1f7102b198e00
Katya Kos <catherine.pths@gmail.com>
2020-12-15 11:10:29 +0300

    astnn network

--- src/encoders/astnn_encoder.py
+++ src/encoders/astnn_encoder.py
@@ -18,105 +18,121 @@ BRACES = ['(', ')', '{', '}', '[', ']']
 
 def _lineraize_non_block_tree_bfs(root: data_pipeline.TreeNode, nodes_queued: int,
                                   max_nodes: int = -1) -> Tuple[List[data_pipeline.TreeNode], List[List[int]]]:
-  nodes: List[data_pipeline.TreeNode] = []
-  children: List[List[int]] = []
-  node_queue = collections.deque()
-  nodes_queued += _try_to_queue_node(root, node_queue, nodes_queued, max_nodes)
-  while node_queue:
-    node = node_queue.popleft()
-    node_children: List[int] = []
-    for child in node['children']:
-      if child in BRACES or child in STATEMENTS:
-        continue
-      if _try_to_queue_node(child, node_queue, nodes_queued, max_nodes):
-        node_children.append(nodes_queued)
-        nodes_queued += 1
-    nodes.append(node)
-    children.append(node_children)
-  return nodes, children
+    nodes: List[data_pipeline.TreeNode] = []
+    children: List[List[int]] = []
+    node_queue = collections.deque()
+    nodes_queued += _try_to_queue_node(root, node_queue, nodes_queued, max_nodes)
+    while node_queue:
+        node = node_queue.popleft()
+        if node is str:
+            nodes.append(node)
+            children.append([])
+        node_children: List[int] = []
+        for child in node['children']:
+            if child in BRACES or child in STATEMENTS:
+                continue
+            if _try_to_queue_node(child, node_queue, nodes_queued, max_nodes):
+                node_children.append(nodes_queued)
+                nodes_queued += 1
+        nodes.append(node)
+        children.append(node_children)
+    return nodes, children
 
 
 def _get_tree_blocks(root: data_pipeline.TreeNode) -> List[data_pipeline.TreeNode]:
-  blocks: List[data_pipeline.TreeNode] = []
-  node_queue = collections.deque()
-  node_queue.append(root)
-  while node_queue:
-    node = node_queue.popleft()
-    for child in node['children']:
-      if child in STATEMENTS:
-        node_queue.append(child)
-    blocks.append(node)
-  return blocks
+    blocks: List[data_pipeline.TreeNode] = []
+    node_queue = collections.deque()
+    node_queue.append(root)
+    while node_queue:
+        node = node_queue.popleft()
+        if node is str:
+            blocks.append(node)
+            continue
+        for child in node['children']:
+            if child in STATEMENTS:
+                node_queue.append(child)
+        if node['type'] == 'compound_statement':
+            node_queue.append('end')
+        blocks.append(node)
+    return blocks
 
 
 def _linearize_and_split_tree_bfs(
     root: data_pipeline.TreeNode,
     max_nodes: int = -1) -> Tuple[List[List[data_pipeline.TreeNode]], List[List[List[int]]]]:
-  nodes: List[List[data_pipeline.TreeNode]] = []
-  children: List[List[List[int]]] = []
-  blocks = _get_tree_blocks(root)
-  nodes_queued = 0
-  for b in blocks:
-    bnodes, bchildren = _lineraize_non_block_tree_bfs(b, nodes_queued, max_nodes)
-    nodes.append(bnodes)
-    children.append(bchildren)
-    if max_nodes != -1 and nodes_queued >= max_nodes:
-      break
-  return nodes, children
+    nodes: List[List[data_pipeline.TreeNode]] = []
+    children: List[List[List[int]]] = []
+    blocks = _get_tree_blocks(root)
+    nodes_queued = 0
+    for b in blocks:
+        bnodes, bchildren = _lineraize_non_block_tree_bfs(b, nodes_queued, max_nodes)
+        nodes.append(bnodes)
+        children.append(bchildren)
+        if max_nodes != -1 and nodes_queued >= max_nodes:
+            break
+    return nodes, children
 
 
 
 class ASTNNEncoder(ASTEncoder):
-  @classmethod
-  def get_default_hyperparameters(cls) -> Dict[str, Any]:
-    hypers = super().get_default_hyperparameters()
-    return hypers
+    @classmethod
+    def get_default_hyperparameters(cls) -> Dict[str, Any]:
+        encoder_hypers = {
+            'tree_encoder_size': 128,
+            # 'tree_hidden_size': 100
+            # we're considering that hidden_size = embedding_size // 2,
+            # so that final representation size (after bigru) would be equal to embedding_size
+        }
+        hypers = super().get_default_hyperparameters()
+        hypers.update(encoder_hypers)
+        return hypers
 
-  def __init__(self, label: str, hyperparameters: Dict[str, Any], metadata: Dict[str, Any]):
-    super().__init__(label, hyperparameters, metadata)
-    assert label == 'code', 'ASTNNEncoder should only be used for code'
+    def __init__(self, label: str, hyperparameters: Dict[str, Any], metadata: Dict[str, Any]):
+        super().__init__(label, hyperparameters, metadata)
+        assert label == 'code', 'ASTNNEncoder should only be used for code'
 
-  def make_model(self, is_train: bool = False) -> tf.Tensor:
-    with tf.variable_scope('astnn_encoder'):
-      self._make_placeholders()
+    def make_model(self, is_train: bool = False) -> tf.Tensor:
+        with tf.variable_scope('astnn_encoder'):
+            self._make_placeholders()
 
-      nodes = self.embedding_layer(self.placeholders['node_type_ids'])
-      children = self.placeholders['children']
-      hidden = astnn_network.init_net(nodes, children, self.get_hyper('type_embedding_size'))
-    return hidden
+            nodes = self.embedding_layer(self.placeholders['node_type_ids'])
+            children = self.placeholders['children']
+            hidden = astnn_network.init_net(nodes, children, self.get_hyper('type_embedding_size'),
+                                            self.get_hyper('type_encoder_size'))
+        return hidden
 
-  def _make_placeholders(self):
-    super()._make_placeholders()
-    self.placeholders['node_type_ids'] = tf.placeholder(tf.int32, shape=[None, None, None], name='node_type_ids')
-    self.placeholders['children'] = tf.placeholder(tf.int32, shape=(None, None, None, None), name='children')
+    def _make_placeholders(self):
+        super()._make_placeholders()
+        self.placeholders['node_type_ids'] = tf.placeholder(tf.int32, shape=[None, None, None], name='node_type_ids')
+        self.placeholders['children'] = tf.placeholder(tf.int32, shape=(None, None, None, None), name='children')
 
-  @classmethod
-  def load_data_from_sample(cls, encoder_label: str, hyperparameters: Dict[str, Any], metadata: Dict[str, Any],
-                            data_to_load: Any, function_name: Optional[str], result_holder: Dict[str, Any],
-                            is_test: bool = True) -> bool:
-    nodes, children = _linearize_and_split_tree_bfs(data_to_load, hyperparameters[f'{encoder_label}_max_num_nodes'])
+    @classmethod
+    def load_data_from_sample(cls, encoder_label: str, hyperparameters: Dict[str, Any], metadata: Dict[str, Any],
+                              data_to_load: Any, function_name: Optional[str], result_holder: Dict[str, Any],
+                              is_test: bool = True) -> bool:
+        nodes, children = _linearize_and_split_tree_bfs(data_to_load, hyperparameters[f'{encoder_label}_max_num_nodes'])
 
-    def convert_and_pad(nodes_):
-      n = len(nodes_)
-      node_types = [node['type'] for node in nodes_]
-      node_type_ids, mask = (
-        tfutils.convert_and_pad_token_sequence(
-          metadata['type_vocab'],
-          node_types,
-          n))
-      assert len(node_type_ids) == n
-      assert np.all(mask == 1)
-      return list(node_type_ids)
+        def convert_and_pad(nodes_):
+            n = len(nodes_)
+            node_types = [node['type'] for node in nodes_]
+            node_type_ids, mask = (
+                tfutils.convert_and_pad_token_sequence(
+                    metadata['type_vocab'],
+                    node_types,
+                    n))
+            assert len(node_type_ids) == n
+            assert np.all(mask == 1)
+            return list(node_type_ids)
 
-    node_type_ids = [convert_and_pad(nodes_) for nodes_ in nodes]
-    result_holder[f'{encoder_label}_node_type_ids'] = list(node_type_ids)
-    result_holder[f'{encoder_label}_children'] = children
-    return True
+        node_type_ids = [convert_and_pad(nodes_) for nodes_ in nodes]
+        result_holder[f'{encoder_label}_node_type_ids'] = list(node_type_ids)
+        result_holder[f'{encoder_label}_children'] = children
+        return True
 
-  def minibatch_to_feed_dict(self, batch_data: Dict[str, Any], feed_dict: Dict[tf.Tensor, Any], is_train: bool) -> None:
-    super().minibatch_to_feed_dict(batch_data, feed_dict, is_train)
-    node_type_ids = batch_data['node_type_ids']
-    children = batch_data['children']
-    node_type_ids, children = astnn_network.pad_batch(node_type_ids, children)
-    tfutils.write_to_feed_dict(feed_dict, self.placeholders['node_type_ids'], node_type_ids)
-    tfutils.write_to_feed_dict(feed_dict, self.placeholders['children'], children)
+    def minibatch_to_feed_dict(self, batch_data: Dict[str, Any], feed_dict: Dict[tf.Tensor, Any], is_train: bool) -> None:
+        super().minibatch_to_feed_dict(batch_data, feed_dict, is_train)
+        node_type_ids = batch_data['node_type_ids']
+        children = batch_data['children']
+        node_type_ids, children = astnn_network.pad_batch(node_type_ids, children)
+        tfutils.write_to_feed_dict(feed_dict, self.placeholders['node_type_ids'], node_type_ids)
+        tfutils.write_to_feed_dict(feed_dict, self.placeholders['children'], children)
--- src/encoders/utils/astnn_network.py
+++ src/encoders/utils/astnn_network.py
@@ -2,44 +2,133 @@ import math
 import tensorflow as tf
 
 
-def init_net(nodes, children, feature_size):
-  """Initialize an empty network."""
+def init_net(nodes, children, feature_size, encoder_size):
+    """Initialize an empty network."""
 
-  with tf.name_scope('network'):
-    tree_enc = tree_encoder_layer(feature_size, nodes, children)
-    hidden = None
+    with tf.name_scope('network'):
+        # (batch_size x split_size x encoder_size)
+        enc = encoder_layer(nodes, children, feature_size, encoder_size)
 
-  with tf.name_scope('summaries'):
-    tf.summary.scalar('split_size', tf.shape(nodes)[1])
-    tf.summary.scalar('tree_size', tf.shape(nodes)[2])
-    tf.summary.scalar('child_size', tf.shape(children)[3])
-    tf.summary.histogram('logits', hidden)
-    tf.summary.image('inputs', tf.expand_dims(nodes, axis=3))
+        # (batch_size x split_size x feature_size)
+        gru_out = bigru_layer(enc, encoder_size, feature_size // 2, 1)
+        # (batch_size x feature_size x split_size)
+        gru_out = tf.transpose(gru_out, perm=[0, 2, 1])
 
-  return hidden
+        with tf.name_scope('pooling'):
+            # (batch_size x feature_size)
+            hidden = tf.reduce_max(gru_out, axis=1)
+
+    with tf.name_scope('summaries'):
+        tf.summary.scalar('split_size', tf.shape(nodes)[1])
+        tf.summary.scalar('tree_size', tf.shape(nodes)[2])
+        tf.summary.scalar('child_size', tf.shape(children)[3])
+        tf.summary.histogram('logits', hidden)
+        tf.summary.image('inputs', tf.expand_dims(nodes, axis=4))
+        tf.summary.image('enc', tf.expand_dims(enc, axis=4))
+        tf.summary.image('bigru', tf.expand_dims(gru_out, axis=4))
+
+    return hidden
 
 
-def tree_encoder_layer(features_size, nodes, children):
-  pass
+def bigru_layer(encodes, input_dim, hidden_dim, num_layers):
+    batch_size = tf.shape(encodes)[0]
+    hidden = tf.Variable(tf.zeros((num_layers * 2, batch_size, hidden_dim)))
+    # bigru i can't even omg
+
+
+def encoder_layer(nodes, children, features_size, encoder_size):
+    with tf.name_scope('encoder_layer'):
+        with tf.name_scope('encoder_layer'):
+            batch_size, split_size = tf.shape(nodes)[0], tf.shape(nodes)[1]
+            splits, split_children = [], []
+            for batch_id in range(batch_size):
+                for split_id in range(split_size):
+                    splits.append(nodes[batch_id][split_id])
+                    split_children.append(children[batch_id][split_id])
+
+        with tf.name_scope('encoder_mul'):
+            node_list = []
+            traverse_mul(splits, split_children, node_list,
+                         features_size, encoder_size)
+
+        with tf.name_scope('encoder_pooling'):
+            node_list = tf.stack(node_list)
+            # (batch_size * split_size x encoder_size)
+            enc_node = tf.reduce_max(node_list, axis=0)
+
+        with tf.name_scope('encoder_outputs'):
+            seq, start, end = [], 0, 0
+            for i in range(batch_size):
+                end += len(nodes[i])
+                seq.append(enc_node[start:end])
+                start = end
+            enc_node = tf.concat(seq, axis=0, name='splits_encodings')
+            # (batch_size x split_size x encoder_size)
+            enc_node = tf.reshape(enc_node, (batch_size, split_size, encoder_size))
+        return enc_node
+
+
+def traverse_mul(nodes, children, node_list, features_size, encoder_size):
+    with tf.name_scope('split_encoder_node'):
+        batch_size = tf.shape(nodes)[0]
+        nodes_size = tf.shape(nodes)[1]
+        for j in range(nodes_size - 1, -1, -1):
+            # (batch_size x 1 x max_children x encoder_size)
+            current_children = tf.stack(
+                [[tf.stack(
+                    [W_c(nodes[i, c, :], features_size, encoder_size) for c in children[i][j]]
+                )] for i in range(batch_size)],
+                name='children_vectors'
+            )
+
+            # (batch_size x 1 x encoder_size)
+            children_sum = tf.reduce_sum(current_children, axis=2, name='children_sum')
+            # (batch_size x encoder_size)
+            new_node_vector = tf.reduce_sum(
+                tf.concat([nodes[:, j:j+1, :], children_sum], axis=1),
+                axis=1, name='new_node_enc'
+            )
+            nodes[:, j, :] = new_node_vector
+            node_list.append(new_node_vector)
+
+
+def W_c(encoded, input_size, output_size):
+    with tf.name_scope('W_c'):
+        weights = tf.Variable(
+            tf.truncated_normal(
+                [input_size, output_size], stddev=1.0 / math.sqrt(input_size)
+            ),
+            name='weights'
+        )
+
+        init = tf.truncated_normal([output_size, ], stddev=math.sqrt(2.0 / input_size))
+        # init = tf.zeros([output_size,])
+        biases = tf.Variable(init, name='biases')
+
+        with tf.name_scope('summaries'):
+            tf.summary.histogram('weights', [weights])
+            tf.summary.histogram('biases', [biases])
+
+        return tf.matmul(encoded, weights) + biases
 
 
 def pad_batch(node_type_ids, children):
-  if not node_type_ids:
-    return [], []
+    if not node_type_ids:
+        return [], []
 
-  max_splits = max([len(b) for b in node_type_ids])
-  node_type_ids = [b + [[]] * (max_splits - len(b)) for b in node_type_ids]
+    max_splits = max([len(b) for b in node_type_ids])
+    node_type_ids = [b + [[]] * (max_splits - len(b)) for b in node_type_ids]
 
-  max_nodes = max([len(s) for b in node_type_ids for s in b])
-  node_type_ids = [[s + [-1] * (max_nodes - len(s)) for s in b] for b in node_type_ids]
+    max_nodes = max([len(s) for b in node_type_ids for s in b])
+    node_type_ids = [[s + [-1] * (max_nodes - len(s)) for s in b] for b in node_type_ids]
 
-  max_splits = max([len(b) for b in children])
-  children = [b + [[[]]] * (max_splits - len(b)) for b in children]
+    max_splits = max([len(b) for b in children])
+    children = [b + [[[]]] * (max_splits - len(b)) for b in children]
 
-  max_nodes = max([len(s) for b in children for s in b])
-  children = [[s + [[]] * (max_nodes - len(s)) for s in b] for b in children]
+    max_nodes = max([len(s) for b in children for s in b])
+    children = [[s + [[]] * (max_nodes - len(s)) for s in b] for b in children]
 
-  max_children = max([len(n) for b in children for s in b for n in s])
-  children = [[[n + [0] * (max_children - len(n)) for n in s] for s in b] for b in children]
+    max_children = max([len(n) for b in children for s in b for n in s])
+    children = [[[n + [0] * (max_children - len(n)) for n in s] for s in b] for b in children]
 
-  return node_type_ids, children
+    return node_type_ids, children
